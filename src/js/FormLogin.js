beeb48dc63738596ae00d7eb16191338aacfebe4
VuVanLien1503 <117723698+VuVanLien1503@users.noreply.github.com>
2023-04-13 09:11:01 +0700

    Hoan Thanh Login

--- src/js/FormLogin.js
+++ src/js/FormLogin.js
@@ -6,21 +6,30 @@ import * as Yup from 'yup'
 import '../css/Login.css'
 import {useState} from "react";
 import {Link, useNavigate} from "react-router-dom";
+import axios from "axios";
 
 export default function FormLogin() {
+
+    const [text, setText] = useState("");
+    const [user, setUser] = useState([])
+    const [userInput, setUserInput] = useState([])
+
     const navigate = useNavigate()
-    const [status,setStatus] = useState("password")
+    const [status, setStatus] = useState("password")
     const Validation = Yup.object().shape({
         password: Yup.string().required("Bạn chưa nhập mật khẩu!").min(6, "Mật khẩu từ 6 đến 8 ký tự!").max(15, "Mật khẩu từ 6 đến 8 ký tự!"),
         email: Yup.string().required("Bạn cần nhập thông tin!"),
     })
+
+
     return (
         <>
             <Formik
                 initialValues={{
                     email: "",
                     password: ""
-                }}
+                }
+                }
                 onSubmit={(values) => {
                     sendData(values)
                 }}
@@ -46,20 +55,23 @@ export default function FormLogin() {
                                                     <div className="form__field">
                                                         <div className="form__field-container">
                                                             <Field name={'email'} type="email" placeholder="Email(*)"/>
-                                                            <div className={'error__message'}><ErrorMessage name={'email'}/></div>
+                                                            <div className={'error__message'}><ErrorMessage
+                                                                name={'email'}/></div>
                                                         </div>
 
                                                     </div>
 
                                                     <div className="form__field">
                                                         <div className="form__field-container">
-                                                            <Field name={'password'} id="password" type={status} placeholder="Mật khẩu(*)"/>
+                                                            <Field name={'password'} id="password" type={status}
+                                                                   placeholder="Mật khẩu(*)"/>
                                                             <div id="event" className="form__field-items-icon-login"
                                                                  onClick={setStatusPassword}>
                                                                 <i id="eye-open" className="fa-solid fa-eye"></i>
                                                                 <i id="eye-close" className="fa-solid fa-eye-slash"></i>
                                                             </div>
-                                                            <div className={'error__message'}><ErrorMessage name={'password'}/></div>
+                                                            <div className={'error__message'}><ErrorMessage
+                                                                name={'password'}/></div>
                                                         </div>
                                                     </div>
 
@@ -67,13 +79,15 @@ export default function FormLogin() {
                                                         <div className="row">
                                                             <div className="col l-6">
                                                                 <div className="container__btn">
-                                                                    <button type={"submit"} className="btn">Đăng nhập</button>
+                                                                    <button type={"submit"} className="btn">Đăng nhập
+                                                                    </button>
                                                                 </div>
                                                             </div>
 
                                                             <div className="col l-6">
                                                                 <div className="container__btn">
-                                                                    <div className="btn" onClick={backHome}>Quay lại</div>
+                                                                    <div className="btn" onClick={backHome}>Quay lại
+                                                                    </div>
                                                                 </div>
                                                             </div>
                                                         </div>
@@ -83,7 +97,8 @@ export default function FormLogin() {
                                                     </div>
 
                                                     <div className="footer__form">
-                                                        <span className="footer__form-text">Bạn mới biết đến FCBlue?</span>
+                                                        <span
+                                                            className="footer__form-text">Bạn mới biết đến FCBlue?</span>
                                                         <Link to={"/register"}>Đăng ký</Link>
                                                     </div>
                                                 </div>
@@ -102,13 +117,13 @@ export default function FormLogin() {
     )
 
     // Ẩn - hiện mật khẩu
-    function setStatusPassword (){
-        if (status === "password"){
+    function setStatusPassword() {
+        if (status === "password") {
             setStatus("text");
             document.getElementById("eye-open").style.display = "block";
             document.getElementById("eye-close").style.display = "none";
 
-        }else {
+        } else {
             setStatus("password");
             document.getElementById("eye-open").style.display = "none";
             document.getElementById("eye-close").style.display = "block";
@@ -116,6 +131,19 @@ export default function FormLogin() {
     }
 
     function sendData(values) {
+        console.log(values)
+        axios.post(`http://localhost:8080/accounts/login`, values).then((response) => {
+            setUser(response.data.user)
+            console.log(response.data.user)
+            setText(response.data.text)
+            console.log(response.data.text)
+            if (response.data.text === "Đăng Nhập Thành Công") {
+                navigate("/")
+            } else {
+                alert(response.data.text)
+            }
+
+        })
 
     }
 
