d51e38a8e1267e9e84aa471df4a3444796db3021
VuVanLien1503 <117723698+VuVanLien1503@users.noreply.github.com>
2023-04-13 17:30:11 +0700

    xác định role của user . tạo mới cửa hàng khi chưa có

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Link, Route, Routes} from "react-router-dom";
 import FormRegister from "./js/FormRegister";
 import FormLogin from "./js/FormLogin";
 import PageShop from "./js/PageShop";
+import CreateShop from "./js/CreateShop";
 
 function App() {
     return (
@@ -15,6 +16,7 @@ function App() {
                 <Route path="/register" element={<FormRegister/>}/>
                 <Route path={"/login"} element={<FormLogin/>}/>
                 <Route path={"/"} element={<PageShop/>}/>
+                <Route path={"/createShop/:id"} element={<CreateShop/>}/>
             </Routes>
         </>
     );
