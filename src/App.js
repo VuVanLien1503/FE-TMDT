6285c6a5443596895b055839d122af23be3efe4f
trmanhthang <thangmt868@gmail.com>
2023-04-14 14:22:03 +0700

    update page home

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Link, Route, Routes} from "react-router-dom";
 import FormRegister from "./js/FormRegister";
 import FormLogin from "./js/FormLogin";
 import PageShop from "./js/PageShop";
+import PageHome from "./js/PageHome";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             {/*<FormRegister/>*/}
             {/*<Link to={"/register"}>Đăng ký</Link>*/}
             {/*<PageShop/>*/}
+            <PageHome/>
             <Routes>
                 <Route path="/register" element={<FormRegister/>}/>
                 <Route path={"/login"} element={<FormLogin/>}/>
